3491d70db1c7ade2b09d42249985c512c84a08c4
Evgeny Belosets <evgenybelorusets@gmail.com>
2015-10-04 16:36:50 +0300

    added user_uid as prefix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :client_applications, only: :show
 
   namespace :api do
-    namespace :v1 do
+    namespace :v1, path: 'v1/:user_uid' do
       resources :users, except: [ :new, :edit ]
       resources :posts, except: [ :new, :edit ] do
         resources :comments, except: [ :new, :edit ]
--- spec/support/shared_examples/restful_controller_spec.rb
+++ spec/support/shared_examples/restful_controller_spec.rb
@@ -6,13 +6,13 @@ RSpec.shared_examples 'RESTful controller' do |params = {}|
   context 'rescue from' do
     it 'should render response with 403 HTTP status if action raised CanCan::AccessDenied' do
       allow(controller).to receive(:authorize!) { raise CanCan::AccessDenied }
-      get :index, params
+      get :index, params.merge(user_uid: 'test')
       expect(response.status).to eql 403
     end
 
     it 'should render response with 403 HTTP status if action raised ActiveRecord::RecordNotFound' do
       allow(controller).to receive(:authorize!) { raise ActiveRecord::RecordNotFound }
-      get :index, params
+      get :index, params.merge(user_uid: 'test')
       expect(response.status).to eql 404
     end
   end
