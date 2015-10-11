f466ee47a6fa8f860b0b0aea3df126e4be9239e9
Evgeny Belosets <evgenybelorusets@gmail.com>
2015-10-11 14:02:57 +0300

    Code Review. Rewritten devise_helper to Rails HTML DSL

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,13 +3,10 @@ module DeviseHelper
     return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    html = <<-HTML
-    <div class="alert alert-error alert-block"> <button type="button"
-    class="close" data-dismiss="alert">x</button>
-      #{messages}
-    </div>
-    HTML
+    button = content_tag :button, 'x', class: 'close', type: 'button', data: { dismiss: 'alert' }
 
-    html.html_safe
+    content_tag :div, class: 'alert alert-error alert-block' do
+      [ button, messages ].join.html_safe
+    end
   end
 end
